b61c4160cd85d0f6a02b3d640abce252921e7c41
MuhammadSarfrazAfzal <sarfrazafzal790@gmail.com>
2025-07-18 22:54:32 +0500

    Used Winston to track the coming reqest to express Api endpoint

--- Logging/Winston.js
+++ Logging/Winston.js
@@ -1,13 +1,37 @@
 
 const { createLogger, format, transports} = require('winston');
+const { combine, timestamp, label, printf } = format;
 
 const logger = createLogger({
     level:'info',
-    format:format.json(),
+    format:format.combine(
+        label({label:'file logs'}),
+        timestamp({format:'YYYY-MM-DD HH:mm:ss'}),
+        printf(({ level, message, label, timestamp }) => {
+            return `${timestamp} [${label}] ${level}: ${message}`;
+          })
+    ),
     transports: [
         new transports.Console(),
         new transports.File({filename:'App.log'})
     ]
     
 })
+
+const ApiLogs = createLogger({
+    level:'info',
+    format:format.combine(
+        label({label:'Api logs'}),
+        timestamp({format:'YYYY-MM-DD HH:mm:ss'}),
+        printf(({ level, message, label, timestamp }) => {
+            return `${timestamp} [${label}] ${level}: ${message}`;
+          })
+    ),
+    transports: [
+        new transports.Console(),
+        new transports.File({filename:'Apilogs.log'})
+    ]
+    
+})
 module.exports = logger;
+module.exports = ApiLogs;
--- Logging/index.js
+++ Logging/index.js
@@ -1,3 +1,15 @@
+const ApiLogs = require("./Winston");
 const logger = require("./Winston");
+const express = require("express")
+const app = express()
 
-logger.info("All is going well, don't need to worry")
+app.get('/', (req, res) => {
+  res.send('Hello World')
+  ApiLogs.info("Response sent from home request")
+})
+
+app.get('/getAppLog', (req, res) => {
+    res.send('Currently have no Logs')
+    ApiLogs.info("Response sent to /getAppLog api request")
+  })
+app.listen(3000)
